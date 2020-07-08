2521cf5f0c910dcaf6ab3175d734c27e13f22028
Gerardo López <gerardwjones@gmail.com>
2020-07-07 22:42:20 -0500

    express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('works')
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res){
+    console.log('access from index')
+    res.send('working')
+})
+
+const port = 3001
+const server = app.listen(port, function(){
+    console.log(`Server listening on localhost:${port}`)
+})
