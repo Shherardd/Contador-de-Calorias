7735e3e9efb87bbaa2ab94b4ae4403b3fb144dee
Gerardo López <gerardwjones@gmail.com>
2020-07-09 22:36:33 -0500

    getting values

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+let description = document.getElementById('description').value
+let calories = document.getElementById('calories').value
+let carbs = document.getElementById('carbs').value
+let protein = document.getElementById('carbs').value
+
+
