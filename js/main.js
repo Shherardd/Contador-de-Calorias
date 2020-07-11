bb1d3e54aa97dd023e85d7fa8ba3ee542fd84953
Gerardo López <gerardwjones@gmail.com>
2020-07-10 23:30:30 -0500

    Reference Objects & Object.Assign

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ let carbs = document.getElementById('carbs').value
 let protein = document.getElementById('carbs').value
 
 
+
+let car = {
+    color: 'red',
+    km: 0,
+    year: 2019
+}
+// Copia la referencia al objetos, por lo que si se modifica uno, los cambios dse ven reflejados
+// en ambos objetos.
+let newCar = new car
+
+//Copia el objeto, por lo que si se realizan cambios son excluyentes al objeto original
+let newCar2 = Object.assign({}, car)
