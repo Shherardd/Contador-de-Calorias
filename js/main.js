21b3b66967263e5ee272f200080704fb76ddd4c6
Gerardo López <gerardwjones@gmail.com>
2020-07-13 20:23:01 -0500

    validation w/ map function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
 
-let description = document.getElementById('description').value
-let calories = document.getElementById('calories').value
-let carbs = document.getElementById('carbs').value
-let protein = document.getElementById('carbs').value
+let description = document.getElementById('description')
+let calories = document.getElementById('calories')
+let carbs = document.getElementById('carbs')
+let protein = document.getElementById('protein')
+let btn = document.getElementById('btn')
+let arr = [description, calories, carbs, protein]
 
-description.addEventListener('click', invalid)
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+    if(validation2(arr) === true){
+        alert('Request enviado de forma exitosa')
+    }else{
+        alert('Faltan campos por rellenar')
+    }    
+})
 
-function invalid () {
-    console.log('ye')
+const validation2 = (arr) => {
+    let state = 0
+    arr.map((x) => {
+        x.value === '' ? x.classList.add('is-invalid') : state++
+    })
+    if(state === 4){return true}
+    else{return false}
 }
 
-let car = {
+description.addEventListener('focus', ()=> description.classList.remove('is-invalid'))
+calories.addEventListener('focus', ()=> calories.classList.remove('is-invalid'))
+carbs.addEventListener('focus', ()=> carbs.classList.remove('is-invalid'))
+protein.addEventListener('focus', ()=> protein.classList.remove('is-invalid'))
+
+/*let car = {
     color: 'red',
     km: 0,
     year: 2019
-}
+}*/
 // Copia la referencia al objetos, por lo que si se modifica uno, los cambios dse ven reflejados
 // en ambos objetos.
-let newCar = new car
+/*let newCar = new car
 
 //Copia el objeto, por lo que si se realizan cambios son excluyentes al objeto original
 let newCar2 = Object.assign({}, car)
@@ -25,4 +44,4 @@ let newCar2 = Object.assign({}, car)
 //parse convierte un string JSON a un objeto de JS
 //stringify convierte un objeto JS  a un String JSON
 //Esta variable copia objetos anidados dentro de otros objetos, sin solo copiar sus referencias
-let NewCar3 = JSON.parse(JSON.stringify(car))
+let NewCar3 = JSON.parse(JSON.stringify(car))*/
